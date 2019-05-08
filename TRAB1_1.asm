c5858593ac1efec378a1f8d3dfc1424ca560125f
Thiago <topd97@yahoo.com.br>
2019-05-08 15:12:23 -0300

    quase

--- TRAB1_1.asm
+++ TRAB1_1.asm
@@ -4,18 +4,27 @@
 ; Data: 14/04/2019
 ;---------------------------------------------------
 
-ORG 100h
+ORG 200h
 
 ;Declarando variaveis
-
-teste1Alta:       DB   0FEh      ;00010000b
-teste1Baixa:      DB   0FFh      ;11111111b
-teste2Alta:       DB   0FEh      ;00010000b
-teste2Baixa:      DB   0FFh      ;00010010b
+                       
+teste1Baixa:      DB   21h
+teste1Alta:       DB   70h
+teste2Baixa:      DB   0F3h
+teste2Alta:       DB   1Fh
+endTeste1:        DW   teste1Baixa
+endTeste2:        DW   teste2Baixa
+endTemp:          DS   2
+endRet:           DS   2
+endRetGeral:      DS   2
+tempBaixa:        DB   0
+tempAlta:         DB   0
+podeUnder:        DB   0
+deuOVer:          DB   0
 precisaDesinverter: DB 0h
-somar               DB 0h
-retAlto           DS       1
-retBaixo          DS       1
+somar:              DB 0h
+retAlto:           DS       1
+retBaixo:          DS       1
 var1Alta:         DS       1
 var1Baixa:        DS       1
 var2Alta:         DS       1
@@ -23,40 +32,109 @@ var2Baixa:        DS       1
 resultBaixa:      DS       1
 resultAlta:       DS       1
 VISOR:            EQU      0
+a:                DB       0
+b:                DB       0
+c:                DB       0
+d:                DB       0
+e:                DB       0
+f:                DB       0
+
 
 ORG 0
-INICIO: ;Inicialização de variaveis para a pilha
-        LDA teste1Alta
+INICIOEXEMPLO: ;Inicialização de variaveis para a pilha
+        LDA endTeste1+1
         PUSH
-        LDA teste1Baixa
+        LDA endTeste1
         PUSH
-        LDA teste2Alta
+        LDA endTeste2+1
         PUSH
-        LDA teste2Baixa
+        LDA endTeste2
         PUSH
         LDA #0
-        JMP ROTINA
+        JSR ROTINA
+        HLT
+
+
 ROTINA:;Inicio verdadeiro da rotina do exercicio
        ADD #0
        STA  somar
+
+       POP
+       STA endRetGeral
+       POP
+       STA endRetGeral+1
+
+
+       POP
+       STA endTemp
+       POP
+       STA endTemp+1
+       LDA @endTemp
+       STA var2Baixa
+
+       LDA endTemp
+       ADD #1
+       STA endTemp
+       LDA @endTemp
+       STA var2Alta
+
+       POP
+       STA endTemp
+       POP
+       STA endTemp+1
+       LDA @endTemp
+       STA var1Baixa
+
+       LDA endTemp
+       ADD #1
+       STA endTemp
+       LDA @endTemp
+       STA var1Alta
+
+       LDA somar
+       JZ CONTINUA
+       LDA var2Alta
+       PUSH
+       LDA var2Baixa
+       PUSH
+       JSR INVERTE
        POP
        STA var2Baixa
        POP
        STA var2Alta
+
+CONTINUA:
+       LDA var1Alta
+       AND var2Alta
+       AND #80h       ;#10000000b
+       JZ CONTINUASOMA
+
+       LDA podeUnder  ;como ambos sao negativos pode dar Underflow
+       ADD #1
+       STA podeUnder
+       LDA precisaDesinverter
+       ADD #1
+       STA precisaDesinverter
+
+       LDA var2Alta
+       PUSH
+       LDA var2Baixa
+       PUSH
+       JSR INVERTE
        POP
-       STA var1Baixa
+       STA var2Baixa
        POP
-       STA var1Alta
+       STA var2Alta
+
+       LDA var1Alta
        PUSH
        LDA var1Baixa
        PUSH
        JSR INVERTE
        POP
+       STA var1Baixa
        POP
-       LDA var1Alta
-       AND var2Alta
-       AND #80h       ;#10000000b
-       JNZ INVERTE
+       STA var1Alta
 CONTINUASOMA:
              LDA var1Baixa
              ADD var2Baixa
@@ -64,67 +142,91 @@ CONTINUASOMA:
              LDA var1Alta
              ADC var2Alta
              STA resultAlta
+
+             PUSH
+             JSR VERIFICAOVER
+
              LDA precisaDesinverter
-             JNZ DESINVERTE
+             JZ  IMPRIME
+
+             SUB #1
+             STA precisaDesinverter
+
+             LDA resultAlta
+             PUSH
+             LDA resultBaixa
+             PUSH
+             JSR INVERTE
+             POP
+             STA resultBaixa
+             POP
+             STA resultAlta
              JMP IMPRIME
-SUBTRACAO:
-          POP
-          STA resultBaixa
-          POP
-          STA resultAlta
-          POP
-          SUB resultBaixa
-          STA resultBaixa
-          POP
-          SUB resultAlta
-          STA resultAlta
-          JMP IMPRIME
 
 INVERTE:
-        LDA precisaDesinverter
-        NOT
-        STA precisaDesinverter
         POP
-        STA retAlto
+        STA endRet
         POP
-        STA retBaixo
+        STA endRet+1
         POP
+        STA tempBaixa
+        POP
+        STA tempAlta
+
+        LDA tempBaixa
         NOT
         ADD #1
-        STA var1Baixa
-        LDA var1Alta
+        STA tempBaixa
+        LDA tempAlta
         NOT
         ADC #0
-        STA var1Alta
-        LDA var2Baixa
-        NOT
-        ADD #1
-        STA var2Baixa
-        LDA var2Alta
-        NOT
-        ADC #0
-        STA var2Alta
-        JMP CONTINUASOMA
-DESINVERTE:
-           LDA precisaDesinverter
-           NOT
-           STA precisaDesinverter
-           LDA resultBaixa
-           NOT
-           ADD #1
-           STA resultBaixa
-           LDA resultAlta
-           NOT
-           ADC #0
-           STA resultAlta
+        PUSH
+        LDA tempBaixa
+        PUSH
+
+        LDA endRet+1
+        PUSH
+        LDA endRet
+        PUSH
+        RET
+
 IMPRIME:
+
         LDA resultAlta
         OUT VISOR
         LDA resultBaixa
         OUT VISOR
 
+        ;colocar o retorno na pilha
 
-END
+        LDA endRetGeral+1
+        PUSH
+        LDA endRetGeral
+        PUSH
+        LDA deuOver
+        RET
+
+VERIFICAOVER:
+             POP
+             STA endRet
+             POP
+             STA endRet+1
+
+             POP
+             AND #80h
+             JNZ OVERACONTECEU
+             STA deuOver
+             JMP ENCERRAOVER
+OVERACONTECEU:
+             LDA #1
+             STA deuOver
+ENCERRAOVER:
+             LDA endRet+1
+             PUSH
+             LDA endRet
+             PUSH
+             RET
+
 
 
 
